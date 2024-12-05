175c3fd58f282771d83c0b12d9616bdbd58341de
Michel Buffa <michelbuffa@MacBook-Pro-de-Michel-2.local>
2024-12-05 08:40:14 +0100

    Refactor de l'exemple des boids

--- 7-Boids/boids.js
+++ 7-Boids/boids.js
@@ -4,58 +4,99 @@
 // https://youtu.be/mhjuuHl6qHM
 
 class Boid {
-  constructor() {
-    this.position = createVector(random(width), random(height));
-    this.velocity = p5.Vector.random2D();
-    this.velocity.setMag(random(2, 4));
-    this.acceleration = createVector();
+  static debug = false;
+  constructor(x, y, image) {
+
+    this.pos = createVector(x, y);
+
+    
+
+    this.vel = p5.Vector.random2D();
+    this.vel.setMag(random(2, 4));
+    this.acc = createVector();
     this.maxForce = 0.2;
     this.maxSpeed = 5;
+    this.r = 6;
+
+    // si le boid est une image
+    if (image !== undefined) {
+      this.image = image;
+
+      // largeur image
+      const li = this.image.width;
+      // hauteur image
+      const hi = this.image.height;
+      // on remet les valeurs à l'échelle par rapport au rayon
+      // du véhicule
+      const ratio = li / hi;
+      // la largeur de l'image sera égale à r
+      this.imageL = this.r;
+      // la hauteur de l'image sera égale à r/ratio
+      this.imageH = this.r / ratio;
+    }
+
+    // pour le comportement align
+    this.alignWeight = 1.5;
+    this.perceptionRadius = 25;
+    // pour le comportement cohesion
+    this.cohesionWeight = 1;
+    // Pour la séparation
+    this.separationWeight = 2;
+    // Pour le confinement
+    this.boundariesWeight = 10;
   }
 
-  edges() {
-    if (this.position.x > width) {
-      this.position.x = 0;
-    } else if (this.position.x < 0) {
-      this.position.x = width;
-    }
-    if (this.position.y > height) {
-      this.position.y = 0;
-    } else if (this.position.y < 0) {
-      this.position.y = height;
-    }
+  // Equivalent de "applyBehaviors" dans le code des autres exemples
+  // flock signifie "se rassembler" en anglais
+  flock(boids) {
+    let alignment = this.align(boids);
+    let cohesion = this.cohesion(boids);
+    let separation = this.separation(boids);
+    let boundaries = this.boundaries(0, 0, width, height, 25);
+    //let boundaries = this.boundaries(100, 200, 800, 400, 25);
+
+    alignment.mult(this.alignWeight);
+    cohesion.mult(this.cohesionWeight);
+    separation.mult(this.separationWeight);
+    boundaries.mult(this.boundariesWeight);
+
+    this.applyForce(alignment);
+    this.applyForce(cohesion);
+    this.applyForce(separation);
+    this.applyForce(boundaries);
   }
 
   align(boids) {
-    let perceptionRadius = 25;
+    let perceptionRadius = this.perceptionRadius;
+
     let steering = createVector();
     let total = 0;
     for (let other of boids) {
-      let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+      let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
       if (other != this && d < perceptionRadius) {
-        steering.add(other.velocity);
+        steering.add(other.vel);
         total++;
       }
     }
     if (total > 0) {
       steering.div(total);
       steering.setMag(this.maxSpeed);
-      steering.sub(this.velocity);
+      steering.sub(this.vel);
       steering.limit(this.maxForce);
     }
     return steering;
   }
 
   separation(boids) {
-    let perceptionRadius = 24;
+    let perceptionRadius = this.perceptionRadius;
 
     let steering = createVector();
     let total = 0;
-    
+
     for (let other of boids) {
-      let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+      let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
       if (other != this && d < perceptionRadius) {
-        let diff = p5.Vector.sub(this.position, other.position);
+        let diff = p5.Vector.sub(this.pos, other.pos);
         diff.div(d * d);
         steering.add(diff);
         total++;
@@ -64,60 +105,155 @@ class Boid {
     if (total > 0) {
       steering.div(total);
       steering.setMag(this.maxSpeed);
-      steering.sub(this.velocity);
+      steering.sub(this.vel);
       steering.limit(this.maxForce);
     }
     return steering;
   }
 
   cohesion(boids) {
-    let perceptionRadius = 50;
+    let perceptionRadius = 2*this.perceptionRadius;
 
     let steering = createVector();
     let total = 0;
 
     for (let other of boids) {
-      let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+      let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
       if (other != this && d < perceptionRadius) {
-        steering.add(other.position);
+        steering.add(other.pos);
         total++;
       }
     }
     if (total > 0) {
       steering.div(total);
 
-      steering.sub(this.position);
+      steering.sub(this.pos);
       steering.setMag(this.maxSpeed);
-      steering.sub(this.velocity);
+      steering.sub(this.vel);
       steering.limit(this.maxForce);
     }
     return steering;
   }
 
-  flock(boids) {
-    let alignment = this.align(boids);
-    let cohesion = this.cohesion(boids);
-    let separation = this.separation(boids);
+  // Permet de rester dans les limites d'une zone rectangulaire.
+  // Lorsque le véhicule s'approche d'un bord vertical ou horizontal
+  // on calcule la vitesse désirée dans la direction "réfléchie" par
+  // rapport au bord (comme au billard).
+  // Par exemple, si le véhicule s'approche du bord gauche à moins de 
+  // 25 pixels (valeur par défaut de la variable d),
+  // on calcule la vitesse désirée en gardant le x du vecteur vitesse
+  // et en mettant son y positif. x vaut maxSpeed et y vaut avant une valeur
+  // négative (puisque le véhicule va vers la gauche), on lui donne un y positif
+  // ça c'est pour la direction à prendre (vitesse désirée). Une fois la direction
+  // calculée on lui donne une norme égale à maxSpeed, puis on calcule la force
+  // normalement : force = vitesseDesiree - vitesseActuelle
+  // paramètres = un rectangle (bx, by, bw, bh) et une distance d
+  boundaries(bx, by,bw, bh, d) {
 
-    alignment.mult(alignSlider.value());
-    cohesion.mult(cohesionSlider.value());
-    separation.mult(separationSlider.value());
+    let vitesseDesiree = null;
 
-    this.acceleration.add(alignment);
-    this.acceleration.add(cohesion);
-    this.acceleration.add(separation);
+    const xBordGauche = bx + d;
+    const xBordDroite = bx + bw - d;
+    const yBordHaut = by + d;
+    const yBordBas = by + bh - d;
+
+    // si le véhicule est trop à gauche ou trop à droite
+    if (this.pos.x < xBordGauche) {
+      // 
+      vitesseDesiree = createVector(this.maxSpeed, this.vel.y);
+    } else if (this.pos.x > xBordDroite) {
+      vitesseDesiree = createVector(-this.maxSpeed, this.vel.y);
+    }
+
+    if (this.pos.y < yBordHaut) {
+      vitesseDesiree = createVector(this.vel.x, this.maxSpeed);
+    } else if (this.pos.y > yBordBas) {
+      vitesseDesiree = createVector(this.vel.x, -this.maxSpeed);
+    }
+
+    if (vitesseDesiree !== null) {
+      vitesseDesiree.setMag(this.maxSpeed);
+      const force = p5.Vector.sub(vitesseDesiree, this.vel);
+      vitesseDesiree.limit(this.maxForce);
+      return vitesseDesiree;
+    }
+
+    if (Boid.debug) {
+      // dessin du cadre de la zone
+      push();
+
+      noFill();
+      stroke("white");
+      rect(bx, by, bw, bh);
+      // et du rectangle intérieur avec une bordure rouge de d pixels
+      stroke("red");
+      rect(bx + d, by + d, bw - 2 * d, bh - 2 * d);
+
+      pop();
+    }
+
+    // si on est pas près du bord (vitesse désirée nulle), on renvoie un vecteur nul
+    return createVector(0, 0);
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
   }
 
   update() {
-    this.position.add(this.velocity);
-    this.velocity.add(this.acceleration);
-    this.velocity.limit(this.maxSpeed);
-    this.acceleration.mult(0);
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.acc.mult(0);
   }
 
   show() {
-    strokeWeight(6);
-    stroke(255);
-    point(this.position.x, this.position.y);
+    if (this.image !== undefined) {
+      imageMode(CENTER);
+
+      // On regarde la direction dans laquelle le boid va :
+      push();
+      translate(this.pos.x, this.pos.y);
+      if (Math.abs(this.vel.x) > Math.abs(this.vel.y)) {
+        if(this.vel.x > 0) {
+          // "L'objet va vers la droite.";
+          rotate(PI);
+        } else {
+          // "L'objet va vers la gauche.";
+          rotate(0);
+        }
+      } else {
+        if (this.vel.y > 0) {
+          rotate(-PI/2)
+          // "L'objet va vers le haut." 
+        } else {
+          rotate(PI/2)
+          // : "L'objet va vers le bas."
+        }
+    }
+      image(this.image, 0, 0, this.r, this.r);
+
+      pop();
+
+      return;
+    } else {
+      strokeWeight(this.r);
+      stroke(255);
+      point(this.pos.x, this.pos.y);
+    }
   }
+
+  edges() {
+    if (this.pos.x > width) {
+      this.pos.x = 0;
+    } else if (this.pos.x < 0) {
+      this.pos.x = width;
+    }
+    if (this.pos.y > height) {
+      this.pos.y = 0;
+    } else if (this.pos.y < 0) {
+      this.pos.y = height;
+    }
+  }
+
 }
--- 7-Boids/sketch.js
+++ 7-Boids/sketch.js
@@ -3,26 +3,99 @@
 // https://thecodingtrain.com/CodingChallenges/124-flocking-boids.html
 // https://youtu.be/mhjuuHl6qHM
 
+// Equivalent du tableau de véhicules dans les autres exemples
 const flock = [];
+let fishImage;
 
 let alignSlider, cohesionSlider, separationSlider;
+let labelNbBoids;
+
+function preload() {
+  // On charge une image de poisson
+  fishImage = loadImage('assets/niceFishtransparent.png');
+}
 
 function setup() {
-  createCanvas(1000, 800);
-  alignSlider = createSlider(0, 2, 1.5, 0.1);
-  cohesionSlider = createSlider(0, 2, 1, 0.1);
-  separationSlider = createSlider(0, 2, 2, 0.1);
+  createCanvas(1600, 800);
+
+  // Quelques sliders pour régler les "poids" des trois comportements de flocking
+  // flocking en anglais c'est "se rassembler"
+  // rappel : tableauDesVehicules, min max val step posX posY propriété
+  const posYSliderDeDepart = 10;
+  creerUnSlider("Poids alignment", flock, 0, 2, 1.5, 0.1, 10, posYSliderDeDepart, "alignWeight");
+  creerUnSlider("Poids cohesion", flock, 0, 2, 1, 0.1, 10, posYSliderDeDepart+30, "cohesionWeight");
+  creerUnSlider("Poids séparation", flock, 0, 15, 3, 0.1, 10, posYSliderDeDepart+60,"separationWeight");
+  creerUnSlider("Rayon des boids", flock, 4, 40, 6, 1, 10, posYSliderDeDepart+90,"r");
+
+
+  // On créer les "boids". Un boid en anglais signifie "un oiseau" ou "un poisson"
+  // Dans cet exemple c'est l'équivalent d'un véhicule dans les autres exemples
   for (let i = 0; i < 200; i++) {
-    flock.push(new Boid());
+    const b = new Boid(random(width), random(height), fishImage);
+    b.r = random(8, 40);
+    flock.push(b);
   }
+
+  // Créer un label avec le nombre de boids présents à l'écran
+   labelNbBoids = createP("Nombre de boids : " + flock.length);
+  // couleur blanche
+  labelNbBoids.style('color', 'white');
+  labelNbBoids.position(10, posYSliderDeDepart+120);
+}
+
+function creerUnSlider(label, tabVehicules, min, max, val, step, posX, posY, propriete) {
+  let slider = createSlider(min, max, val, step);
+  
+  let labelP = createP(label);
+  labelP.position(posX, posY);
+  labelP.style('color', 'white');
+
+  slider.position(posX + 150, posY + 17);
+
+  let valueSpan = createSpan(slider.value());
+  valueSpan.position(posX + 300, posY+17);
+  valueSpan.style('color', 'white');
+  valueSpan.html(slider.value());
+
+  slider.input(() => {
+    valueSpan.html(slider.value());
+    tabVehicules.forEach(vehicle => {
+      vehicle[propriete] = slider.value();
+    });
+  });
 }
 
 function draw() {
   background(0);
+
+    // mettre à jour le nombre de boids
+    labelNbBoids.html("Nombre de boids : " + flock.length);
+
   for (let boid of flock) {
-    boid.edges();
+    //boid.edges();
     boid.flock(flock);
     boid.update();
     boid.show();
   }  
+}
+
+function mouseDragged() {
+  const b = new Boid(mouseX, mouseY, fishImage);
+  
+  b.r = random(8, 40);
+
+  flock.push(b);
+
+
+}
+
+function keyPressed() {
+ if (key === 'd') {
+    Boid.debug = !Boid.debug;
+  } else if (key === 'r') {
+    // On donne une taille différente à chaque boid
+    flock.forEach(b => {
+      b.r = random(8, 40);
+    });
+  }
 }
